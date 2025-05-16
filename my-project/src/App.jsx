3d90486b7438e193ab109ab454aa7560ac436c8b
Adriana <diazmendoadriana04@gmail.com>
2025-05-16 08:34:19 -0500

    Sitio con Nav sin enlaces

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Menu from './components/menu'   
+import HomePage from './pages/HomePage'
+import BlogPage from './pages/BlogPage'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/blog" element={<BlogPage/>}/>
+    </Routes>
+  )
+}
+
+export default App
---
+++ my-project/src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import './Menu.css'
+
+function Menu(){
+  return(
+    <div id='menu'>
+      <h2>Esto es un menú</h2>
+    </div>
+  )
+}
+export default Menu; //El recurso existe
